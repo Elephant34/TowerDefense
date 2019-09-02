46c8b7b5ac0c35bf96584a60119aedb6180e2e51
Ambrose Hlustik-Smith <a.hlustiksmith@gmail.com>
2019-09-02 10:58:06 +0100

    Started making the tower selection screen

--- scenes/HUD/HUD.gd
+++ scenes/HUD/HUD.gd
@@ -1,13 +1,15 @@
 extends Control
 
 
-
-
 func _on_UpArrow_pressed():
 	$MarginContainer/UpArrow.visible = false
 	$MarginContainer/DownArrow.visible = true
+	
+	$TowerSelection.visible = false
 
 
 func _on_DownArrow_pressed():
 	$MarginContainer/UpArrow.visible = true
 	$MarginContainer/DownArrow.visible = false
+	
+	$TowerSelection.visible = true
