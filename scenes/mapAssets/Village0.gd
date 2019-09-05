220ea734ec1e7e123ed1a0b3f3236367c6413943
unknown <a.hlustiksmith@protonmail.com>
2019-09-05 13:02:43 +0100

    Fixed bug in test level and improved village scene

--- scenes/mapAssets/Village0.gd
+++ scenes/mapAssets/Village0.gd
@@ -2,8 +2,14 @@ extends Control
 
 
 func _on_special_pressed(extra_arg_0):
-	print(extra_arg_0)
+	spawn_new_special(extra_arg_0)
 
 
 func _on_resource_pressed(extra_arg_0):
-	print(extra_arg_0)
+	spawn_new_resource(extra_arg_0)
+	
+func spawn_new_resource(position):
+	print("Spawing resource tower at position %s", position)
+
+func spawn_new_special(position):
+	print("Spawning special tower at position %s", position)
