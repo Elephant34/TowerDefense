e2bc1d870bdadc772c76dffb87fce8dda5d0a63d
unknown <a.hlustiksmith@protonmail.com>
2019-09-16 12:21:55 +0100

    Added level base and level 0

---
+++ scenes/levels/LevelBase.gd
@@ -0,0 +1,37 @@
+extends Container
+
+# Tower resouces
+var cannon_resource = preload("res://scenes/towers/cannon/Cannon.tscn")
+
+# Enemy resources
+var standard_resource = preload("res://scenes/ememies/standered/Standered.tscn")
+
+var waves = 0
+var wave_data = [[]]
+
+var current_wave = 0
+
+var mode = "easy"
+
+export var coins = 100
+export var mana = 0
+export var civilians = 30
+
+func _ready():
+	wave_data = wave_data[mode]
+
+func _process(delta):
+	$HUD/Resources/VBoxContainer/Civilians.text = "Civilians: " + str(civilians)
+	$HUD/Resources/VBoxContainer/Coins.text = "Coins: " + str(coins)
+	$HUD/Resources/VBoxContainer/Mana.text = "Mana: " + str(mana)
+	
+	$HUD/Level/VBoxContainer/Wave.text = "Wave: " + str(current_wave) + "/" + str(waves)
+
+func new_tower(type):
+	match type:
+		"cannon":
+			var cannon = cannon_resource.instance()
+			$Towers.add_child(cannon)
+
+func spawn_wave():
+	$Enemies.add_child(standard_resource.instance())
--- scenes/mapAssets/Village0.gd
+++ scenes/mapAssets/Village0.gd
@@ -4,30 +4,31 @@ extends Control
 func _ready():
 	for child in $ZoneMarkers.get_children():
 		child.visible = false
+	
+	match globals.current_zone:
+		0:
+			$ZoneMarkers/Zone0.visible = true
+		1:
+			$ZoneMarkers/Zone1.visible = true
+		2:
+			$ZoneMarkers/Zone2.visible = true
+		3:
+			$ZoneMarkers/Zone3.visible = true
+		4:
+			$ZoneMarkers/Zone4.visible = true
 
-	if globals.current_zone == 0:
-		$ZoneMarkers/Zone0.visible = true
-	if globals.current_zone == 1:
-		$ZoneMarkers/Zone1.visible = true
-	if globals.current_zone == 2:
-		$ZoneMarkers/Zone2.visible = true
-	if globals.current_zone == 3:
-		$ZoneMarkers/Zone3.visible = true
-	if globals.current_zone == 4:
-		$ZoneMarkers/Zone4.visible = true
 
 func _on_special_pressed(extra_arg_0):
 	spawn_new_special(extra_arg_0)
 
+func spawn_new_special(position):
+	print("Spawning special tower at position ", position)
 
 func _on_resource_pressed(extra_arg_0):
 	spawn_new_resource(extra_arg_0)
 	
 func spawn_new_resource(position):
 	print("Spawing resource tower at position ", position)
-
-func spawn_new_special(position):
-	print("Spawning special tower at position ", position)
 
 
 func _on_Zone_pressed(extra_arg_0):
