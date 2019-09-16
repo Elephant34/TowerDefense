9b9eb726ad3c07a492c36c3a1af0bb2ae5d462fa
unknown <a.hlustiksmith@protonmail.com>
2019-09-16 12:31:06 +0100

    linked level0 to village0 zone marker

--- scenes/mapAssets/Village0.gd
+++ scenes/mapAssets/Village0.gd
@@ -5,17 +5,16 @@ func _ready():
 	for child in $ZoneMarkers.get_children():
 		child.visible = false
 	
-	match globals.current_zone:
-		0:
-			$ZoneMarkers/Zone0.visible = true
-		1:
-			$ZoneMarkers/Zone1.visible = true
-		2:
-			$ZoneMarkers/Zone2.visible = true
-		3:
-			$ZoneMarkers/Zone3.visible = true
-		4:
-			$ZoneMarkers/Zone4.visible = true
+	if globals.current_zone == 0:
+		$ZoneMarkers/Zone0.visible = true
+	if globals.current_zone == 1:
+		$ZoneMarkers/Zone1.visible = true
+	if globals.current_zone == 2:
+		$ZoneMarkers/Zone2.visible = true
+	if globals.current_zone == 3:
+		$ZoneMarkers/Zone3.visible = true
+	if globals.current_zone == 4:
+		$ZoneMarkers/Zone4.visible = true
 
 
 func _on_special_pressed(extra_arg_0):
@@ -32,4 +31,7 @@ func spawn_new_resource(position):
 
 
 func _on_Zone_pressed(extra_arg_0):
-	print("loading zone ", extra_arg_0)
+	
+	match extra_arg_0:
+		0:
+			get_tree().change_scene("res://scenes/levels/Level0.tscn")
